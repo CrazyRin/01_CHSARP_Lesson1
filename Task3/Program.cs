ec845bd8e0ac102476dcdb4079d068babe50d473
Crazy Fly <citirin@list.ru>
2024-03-29 16:23:32 +0300

    add Lesson1:Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿//1
+int a = 5;
+int b = 7;
+
+if (a>b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
+
+// 2
+int a1 = 100;
+int b2 = 7;
+
+if (a1>b2)
+{
+    Console.WriteLine(a1);
+}
+else
+{
+    Console.WriteLine(b2);
+}
