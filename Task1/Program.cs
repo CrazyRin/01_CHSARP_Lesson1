03dda825f5b5586d5279636f580f28cf4d098cb5
Crazy Fly <citirin@list.ru>
2024-03-29 15:39:12 +0300

    add some variables lesson1:tsk

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello World");
+﻿//Console.WriteLine("Hello World");
+
+int value = 100;
+Console.WriteLine(value);
+//long value_2 = 100000;
+//Console.WriteLine(value_2);
+//fload value_3 = 0,1;
+//Console.WriteLine(value_3);
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
